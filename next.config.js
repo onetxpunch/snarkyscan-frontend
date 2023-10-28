ff49235734e7d916de9aa8d0134e5158c8bfdae9
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-27 22:44:51 -0400

    react relay deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    relay: {
+      src: "./",
+      language: "typescript",
+      artifactDirectory: "./__generated__",
+    },
+  },
+  optimizeFonts: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { RelayEnvironmentProvider } from "react-relay";
+import { initRelayEnvironment } from "../RelayEnvironment";
+import { RecordSource } from "relay-runtime";
+import { useMemo, useEffect } from "react";
+import { RelayPageProps } from "../relay-types";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<RelayPageProps>) {
+  const environment = useMemo(initRelayEnvironment, []);
+
+  useEffect(() => {
+    const store = environment.getStore();
+
+    // Hydrate the store.
+    store.publish(new RecordSource(pageProps.initialRecords));
+
+    // Notify any existing subscribers.
+    store.notify();
+  }, [environment, pageProps.initialRecords]);
+
+  return (
+    <RelayEnvironmentProvider environment={environment}>
+      <Component {...pageProps} />
+    </RelayEnvironmentProvider>
+  );
 }
