5dc21fb3b623d2dcc78f808e6406d278eb815782
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-28 23:21:24 -0400

    mina fin stats, icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,13 @@
 const Nav = () => {
   return (
     <div className="sticky top-0 flex flex-col justify-center w-full h-16 p-4 mb-8 text-2xl font-black shadow-xl bg-emerald-200">
-      <div>Snarkyscan</div>
-      <div className="-mt-2 text-2xl">explorer</div>
+      <div className="flex items-center gap-2">
+        <div>🔎</div>
+        <div>
+          <div>Snarkyscan</div>
+          <div className="-mt-2 text-2xl">explorer</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,6 +1,9 @@
+import { CoinGeckoClient } from "coingecko-api-v3";
 import { graphql } from "relay-runtime";
 import { useLazyLoadQuery } from "react-relay";
-// import { ListQuery as ListQueryT } from "../../__generated__/ListQuery.graphql";
+import { OverviewQuery as OverviewQueryT } from "../../__generated__/OverviewQuery.graphql";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const OverviewQuery = graphql`
   query OverviewQuery {
@@ -24,13 +27,42 @@ const OverviewQuery = graphql`
   }
 `;
 const Overview = () => {
-  const data = useLazyLoadQuery<any>(OverviewQuery, {});
+  const [lastPrice, setLastPrice] = useState();
+  const [marketCap, setMarketCap] = useState();
+  const data = useLazyLoadQuery<OverviewQueryT>(OverviewQuery, {});
+
+  const formatUSD = (amount) =>
+    new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(amount);
+
+  const fetchPriceInfo = async () => {
+    const client = new CoinGeckoClient({
+      timeout: 10000,
+      autoRetry: true,
+    });
+
+    const price = await client.simplePrice({
+      ids: "mina-protocol",
+      vs_currencies: "usd",
+      include_market_cap: true,
+    });
+    console.log(price);
+    setLastPrice(price["mina-protocol"].usd);
+    setMarketCap(price["mina-protocol"].usd_market_cap);
+  };
+
+  useEffect(() => {
+    fetchPriceInfo();
+  }, []);
+
   return (
     <div className="flex flex-col gap-3 p-4 my-4 shadow-xl rounded-xl bg-slate-200">
       <div>Mina price</div>
-      <div>{data.blocks[0].blockHeight}</div>
+      <div>{formatUSD(lastPrice)}</div>
       <div>Market cap</div>
-      <div>{data.blocks[0].blockHeight}</div>
+      <div>{formatUSD(marketCap)}</div>
       <div>Last finalized block</div>
       <div>{data.blocks[0].blockHeight}</div>
     </div>
