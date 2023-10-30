b0a72343c29c33aab4b5d3f63658a67d07e433ed
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-30 10:52:17 -0400

    other sections

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -47,11 +47,44 @@ const BalanceInfo = ({}) => {
   );
 };
 
+const MoreInfo = ({}) => {
+  const [minaBalance, setMinaBalance] = useState<number>();
+  return (
+    <div className="p-4 bg-white flex flex-col gap-3 border-slate-200 rounded-lg border-[1px]">
+      <div className="text-lg">More Info</div>
+      <div className="flex flex-col gap-1">
+        <div className="text-sm uppercase text-slate-600">Last sent</div>
+        <div>{minaBalance}</div>
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <div className="text-sm uppercase text-slate-600">First sent</div>
+        <div>{minaBalance}</div>
+      </div>
+    </div>
+  );
+};
+
+const OtherChains = ({}) => {
+  const [minaBalance, setMinaBalance] = useState<number>();
+  return (
+    <div className="p-4 bg-white flex flex-col gap-3 border-slate-200 rounded-lg border-[1px]">
+      <div className="text-lg">Other Chains</div>
+      <div className="flex flex-col gap-1">
+        <div className="text-sm uppercase text-slate-600">Active on</div>
+        <div>{minaBalance}</div>
+      </div>
+    </div>
+  );
+};
+
 const Address = ({ address }) => {
   return (
     <div className="flex flex-col gap-4">
       <Top address={address} />
       <BalanceInfo />
+      <MoreInfo />
+      <OtherChains />
     </div>
   );
 };
