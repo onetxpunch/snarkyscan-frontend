32006f9d82772164f3311a845b86e40583cb0fc8
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-30 10:37:44 -0400

    clipboard section

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,5 +1,32 @@
+import { VscClippy } from "@react-icons/all-files/vsc/VscClippy";
+import { VscDeviceCamera } from "@react-icons/all-files/vsc/VscDeviceCamera";
+
+const Top = ({ address }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-2 text-2xl">
+        <div className="font-semibold">Address</div>
+        <VscClippy
+          className="inline cursor-pointer hover:text-slate-800 text-slate-700"
+          onClick={() => {
+            navigator.clipboard.writeText(address);
+          }}
+        />
+        <VscDeviceCamera className="hidden inline cursor-pointer hover:text-slate-800 text-slate-700" />
+      </div>
+      <div className="text-sm bg-white border-[1px] border-slate-200 rounded-lg p-2 text-md">
+        {address}
+      </div>
+    </div>
+  );
+};
+
 const Address = ({ address }) => {
-  return <div>{address}</div>;
+  return (
+    <div>
+      <Top address={address} />
+    </div>
+  );
 };
 
 export default Address;
