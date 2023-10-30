9130ad28b38b08f484f5e3a1e04eab2500137618
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-30 10:47:44 -0400

    balance info section

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,5 +1,6 @@
 import { VscClippy } from "@react-icons/all-files/vsc/VscClippy";
 import { VscDeviceCamera } from "@react-icons/all-files/vsc/VscDeviceCamera";
+import { useState } from "react";
 
 const Top = ({ address }) => {
   return (
@@ -21,10 +22,36 @@ const Top = ({ address }) => {
   );
 };
 
+const BalanceInfo = ({}) => {
+  const [minaBalance, setMinaBalance] = useState<number>();
+  return (
+    <div className="p-4 bg-white flex flex-col gap-3 border-slate-200 rounded-lg border-[1px]">
+      <div className="text-lg">Overview</div>
+      <div className="flex flex-col gap-1">
+        <div className="text-sm uppercase text-slate-600">Mina Balance</div>
+        <div>
+          <img
+            alt="mina logo"
+            src="/mina-logo.png"
+            className="w-5 h-5 rounded-full text-slate-800"
+          />
+          {minaBalance}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <div className="text-sm uppercase text-slate-600">Mina Value</div>
+        <div>${minaBalance}</div>
+      </div>
+    </div>
+  );
+};
+
 const Address = ({ address }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <Top address={address} />
+      <BalanceInfo />
     </div>
   );
 };
