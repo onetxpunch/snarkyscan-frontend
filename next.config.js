350e774b8f6af0ad2070df5938bd468091d6650c
punch <117326984+onetxpunch@users.noreply.github.com>
2023-10-31 22:16:48 -0400

    o1js deps

--- next.config.js
+++ next.config.js
@@ -9,6 +9,21 @@ const nextConfig = {
     },
   },
   optimizeFonts: false,
+  headers: () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
