a0983e15f3d5f1092b0cd9ea8034fc43af8e22df
punch <117326984+onetxpunch@users.noreply.github.com>
2023-11-12 10:48:05 -0500

    lookup input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,42 @@
+import Link from "next/link";
+import { VscSearch } from "@react-icons/all-files/vsc/VscSearch";
+import { useState, useEffect } from "react";
+
 const Search = () => {
+  const [input, setInput] = useState<string>(``);
+  const [link, setLink] = useState(`/`);
+
+  useEffect(() => {
+    if (!isNaN(parseInt(input))) setLink(`/block/${input}`);
+    else if (input.startsWith("5J")) setLink(`/tx/${input}`);
+    else if (input.startsWith("B6")) setLink(`/address/${input}`);
+    else setLink(`/`);
+  }, [input]);
+
   return (
-    <div className="py-4 -mt-8 text-2xl font-semibold tracking-tighter">
-      Mina Blockchain Explorer
+    <div className="flex flex-col gap-2">
+      <div className="py-4 -mt-8 text-2xl font-semibold tracking-tighter">
+        Mina Blockchain Explorer
+      </div>
+      <div className="flex items-center justify-center w-full gap-2">
+        <input
+          type="text"
+          value={input}
+          className="w-full max-w-2xl rounded accent-emerald-500 focus:outline-emerald-500 focus:ring-emerald-500"
+          placeholder="Type an address, block number, tx hash"
+          onChange={(e) => {
+            setInput(`${e.currentTarget.value}`);
+          }}
+        />
+        <Link
+          href={link}
+          className={`rounded-lg w-64 items-center justify-center flex p-2 bg-emerald-400 flex gap-2 hover:bg-emerald-500 cursor-pointer active:bg-emerald-200 select-none ${
+            input?.length == 0 ? "bg-slate-400" : "bg-emerald-500"
+          }`}
+        >
+          <VscSearch /> Lookup
+        </Link>
+      </div>
     </div>
   );
 };
